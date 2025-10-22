c2a980033036b6666db623cfa0cf86448ab87ff2
firos3636 <firosop0786@gmail.com>
2025-10-22 11:07:17 +0530

    third

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+const express = require('express');   // Import Express framework
+const app = express();                // Create an Express app
+const PORT = 3000;                    // Define the port number
 
+// Define a route for the home page
 app.get('/', (req, res) => {
-  res.send('Hello firos,how are you!');
+  res.send('Hello firos, how are!');
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
